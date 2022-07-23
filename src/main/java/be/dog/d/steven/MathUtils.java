8e2ee12d0d9b6d292540cf9c66fdefa42f0eeea0
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2022-07-23 21:59:32 +0200

    :recycle: Formatting

--- src/main/java/be/dog/d/steven/MathUtils.java
+++ src/main/java/be/dog/d/steven/MathUtils.java
@@ -3,22 +3,22 @@ package be.dog.d.steven;
 public class MathUtils {
 
     public double computeCircleArea(int i) {
-        return Math.PI * Math.pow(i,2);
+        return Math.PI * Math.pow(i, 2);
     }
 
-    public int add(int a, int b){
+    public int add(int a, int b) {
         return a + b;
     }
 
-    public int subtract(int a, int b){
+    public int subtract(int a, int b) {
         return a - b;
     }
 
-    public int multiply(int a, int b){
+    public int multiply(int a, int b) {
         return a * b;
     }
 
-    public int divide(int a, int b){
+    public int divide(int a, int b) {
         return a / b;
     }
 }
--- src/test/java/be/dog/d/steven/MathUtilsTest.java
+++ src/test/java/be/dog/d/steven/MathUtilsTest.java
@@ -21,19 +21,19 @@ public class MathUtilsTest {
     // SETUP
 
     @BeforeAll
-    static void beforeAll(){
+    static void beforeAll() {
         System.out.println("MathUtils tests starting!");
     }
 
     @BeforeEach
-    void init(TestInfo testInfo, TestReporter testReporter){
+    void init(TestInfo testInfo, TestReporter testReporter) {
         mathUtils = new MathUtils();
         this.testInfo = testInfo;
         this.testReporter = testReporter;
     }
 
     @AfterEach
-    void cleanUp(){
+    void cleanUp() {
         System.out.println("Cleaning up...");
     }
 
@@ -50,7 +50,7 @@ public class MathUtilsTest {
     @Test
     @Tag("area")
     @DisplayName("Testing circle area calculation \uD83D\uDE08")
-    void mathUtilsComputeCircleAreaTest(){
+    void mathUtilsComputeCircleAreaTest() {
         testReporter.publishEntry("Running " + testInfo.getDisplayName() + " with tags: " + testInfo.getTags());
 
         assertEquals(314.1592653589793, mathUtils.computeCircleArea(10), "Should return the surface area of a circle.");
@@ -59,8 +59,8 @@ public class MathUtilsTest {
     @RepeatedTest(3)
     @Math
     @DisplayName("Testing add method 3 times \ud83d\udc3b")
-    void mathUtilsAddTest(RepetitionInfo repetitionInfo){
-        assertEquals(2,mathUtils.add(1,1),"The add method should add two numbers.");
+    void mathUtilsAddTest(RepetitionInfo repetitionInfo) {
+        assertEquals(2, mathUtils.add(1, 1), "The add method should add two numbers.");
         System.out.println(repetitionInfo.getCurrentRepetition());
     }
 
@@ -68,40 +68,40 @@ public class MathUtilsTest {
     @Math
     @DisplayName("Testing divide method")
     void mathUtilsDivideTest() {
-        assertThrows(ArithmeticException.class, () -> mathUtils.divide(1,0), "Divide by 0 should throw.");
+        assertThrows(ArithmeticException.class, () -> mathUtils.divide(1, 0), "Divide by 0 should throw.");
     }
 
     @Test
     @Math
     @DisplayName("Testing multiply method")
-    void mathUtilsMultiplyTest(){
+    void mathUtilsMultiplyTest() {
         assertAll(
-                () -> assertEquals(4, mathUtils.multiply(2,2)),
-                () -> assertEquals(0, mathUtils.multiply(0,2)),
-                () -> assertEquals(-2, mathUtils.multiply(2,-1))
+                () -> assertEquals(4, mathUtils.multiply(2, 2)),
+                () -> assertEquals(0, mathUtils.multiply(0, 2)),
+                () -> assertEquals(-2, mathUtils.multiply(2, -1))
         );
     }
 
     @Nested
     @Math
     @DisplayName("Testing subtract method")
-    class SubtractTests{
+    class SubtractTests {
 
         @Test
-        void mathUtilsSubtractionPositiveTest(){
-            assertEquals(5, mathUtils.subtract(10,5));
+        void mathUtilsSubtractionPositiveTest() {
+            assertEquals(5, mathUtils.subtract(10, 5));
         }
 
         @Test
-        void mathUtilsSubtractionNegativeTest(){
-            assertEquals(15, mathUtils.subtract(10,-5));
+        void mathUtilsSubtractionNegativeTest() {
+            assertEquals(15, mathUtils.subtract(10, -5));
         }
     }
 
     @ParameterizedTest
-    @ValueSource(ints={-5,-3,3,5,8,14})
+    @ValueSource(ints = {-5, -3, 3, 5, 8, 14})
     @DisplayName("Declaring integers")
-    void shouldDeclareInteger(int integer){
+    void shouldDeclareInteger(int integer) {
         Integer i = integer; // POC for parameterized test, no1 should ever test if declaring an integer works
         assertEquals(integer, i);
     }
@@ -111,18 +111,18 @@ public class MathUtilsTest {
 
     @Test
     @Disabled
-    void dontRun(){
+    void dontRun() {
         System.out.println("You will never see this string.");
     }
 
     @Test
     @EnabledOnOs({OS.MAC})
-    void dontRunButOnMac(){
+    void dontRunButOnMac() {
         System.out.println("You will never see this string on windows or linux.");
     }
 
     @Test
-    void assume(){
+    void assume() {
         boolean serverUp = false; // Not hardcoded IRL
         assumeTrue(serverUp, "Server has to be running for this test.");
         System.out.println("You will never see this string.");
